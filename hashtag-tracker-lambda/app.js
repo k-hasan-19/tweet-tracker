f588f9dfeff89a9d13057e7195fd06256d86e1a3
Kamrul Hasan <k.hasan.rajib@gmail.com>
2020-05-15 17:35:41 +0000

    fix local testing issue

--- hashtag-tracker-lambda/app.js
+++ hashtag-tracker-lambda/app.js
@@ -1,21 +1,21 @@
 'use strict';
-require('dotenv').config()
+const dotenv = require('dotenv').config()
 const Twitter = require('twitter-lite')
 const AWS = require('aws-sdk')
 
 const client = new Twitter({
     // subdomain: "api",
-    consumer_key: process.env.TWITTER_CONSUMER_KEY, // from Twitter.
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET, // from Twitter.
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, // from your User (oauth_token)
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET // from your User (oauth_token_secret)
+    consumer_key: dotenv.parsed.TWITTER_CONSUMER_KEY || process.env.TWITTER_CONSUMER_KEY, // from Twitter.
+    consumer_secret: dotenv.parsed.TWITTER_CONSUMER_SECRET || process.env.TWITTER_CONSUMER_SECRET, // from Twitter.
+    access_token_key: dotenv.parsed.TWITTER_ACCESS_TOKEN_KEY || process.env.TWITTER_ACCESS_TOKEN_KEY, // from your User (oauth_token)
+    access_token_secret: dotenv.parsed.TWITTER_ACCESS_TOKEN_SECRET || process.env.TWITTER_ACCESS_TOKEN_SECRET // from your User (oauth_token_secret)
 });
 
 exports.lambda_handler = async(event, context) => {
     // context.log('JavaScript HTTP trigger function processed a request.');
     let searchResult
-    const hashtag = process.env.HASHTAG || 'CoronaVirus'
-    const tablename = process.env.TABLE_NAME || 'twitter-table'
+    const hashtag = dotenv.parsed.HASHTAG || process.env.HASHTAG
+    const tablename = dotenv.parsed.TABLE_NAME || process.env.TABLE_NAME
 
     try {
         searchResult = await client.get('search/tweets', {
